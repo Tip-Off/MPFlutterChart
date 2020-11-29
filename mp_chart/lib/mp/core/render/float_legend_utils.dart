3980a98e037e5d9bb2ec6ab955f99c335708789d
Eduardo Barreto Alexandre <sezdocs@gmail.com>
2020-11-29 17:33:12 -0300

    Remove commented code

--- mp_chart/lib/mp/core/render/float_legend_utils.dart
+++ mp_chart/lib/mp/core/render/float_legend_utils.dart
@@ -24,6 +24,7 @@ class FloatLegendUtils {
     entryColors.keys.forEach((element) {
       final position = Offset(viewPortHandler.contentLeft(), viewPortHandler.contentTop() + drawSize.height);
       final legendSize = _drawTextLegend(labelText, c, entryColors[element], element, position);
+
       drawSize = Size(drawSize.width + legendSize.width, drawSize.height + legendSize.height);
     });
 
@@ -34,7 +35,6 @@ class FloatLegendUtils {
   static Map<String, List<EntryColor>> _createEntries<T extends IDataSet>(BarLineScatterCandleBubbleData data, List<Highlight> indices) {
     final Map<String, List<EntryColor>> entryColors = {};
 
-    // dataSet = candleData.dataSets.firstWhere((element) => element.getEntriesForXValue(high.x).length > 0, orElse: () => null);
     data.dataSets.where((element) => element is T && element.getEntriesForXValue(indices.first.x).length > 0).toList().asMap().forEach((i, element) {
       if (element.isVisible()) {
         final h = indices.first;
